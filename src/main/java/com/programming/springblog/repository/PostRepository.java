2ae20993bf1acce753660e579ffd8a811eebe8e9
Bartlomiej Puka <bartlomiej.puka@netsoftware.pl>
2020-10-16 14:16:22 +0200

    - addded @Repository annotation in PostRepository.java

--- src/main/java/com/programming/springblog/repository/PostRepository.java
+++ src/main/java/com/programming/springblog/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.programming.springblog.repository;
 
 import com.programming.springblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
