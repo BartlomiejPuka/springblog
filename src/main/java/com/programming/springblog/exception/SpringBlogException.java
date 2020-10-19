df0257b9a59ca606cf64bb63ef585d3f92b82594
Bartlomiej Puka <bartlomiej.puka@netsoftware.pl>
2020-10-19 17:12:57 +0200

    - addded AuthenticationResponse.java
    - installed lombok plugin

---
+++ src/main/java/com/programming/springblog/exception/SpringBlogException.java
@@ -0,0 +1,8 @@
+package com.programming.springblog.exception;
+
+public class SpringBlogException extends RuntimeException{
+
+    public SpringBlogException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/programming/springblog/security/JwtProvider.java
+++ src/main/java/com/programming/springblog/security/JwtProvider.java
@@ -1,42 +1,74 @@
 package com.programming.springblog.security;
 
+import com.programming.springblog.exception.SpringBlogException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
 
-    private Key key;
+    private KeyStore keyStore;
 
     @PostConstruct
     public void init(){
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        try {
+
+            keyStore = KeyStore.getInstance("JKS");
+            Resource resource = new ClassPathResource("springblog.jks");
+            InputStream resourceAsStream = resource.getInputStream();
+//            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e){
+            throw new SpringBlogException("Exception occured while loading keystore");
+        }
     }
 
     public String generateToken(Authentication authentication){
         User principal = (User)authentication.getPrincipal();
         return Jwts.builder()
                     .setSubject(principal.getUsername())
-                    .signWith(key)
+                    .signWith(getPrivateKey())
                     .compact();
     }
-    
+
+    private Key getPrivateKey(){
+        try{
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+        } catch(KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e){
+            throw new SpringBlogException("Exception occured while retrieving public key from keystore");
+        }
+
+    }
+
     public boolean validateToken(String jwt){
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
+    }
+
+    private PublicKey getPublicKey(){
+        try {
+            return keyStore.getCertificate("springblog").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SpringBlogException("Exception occured while retrieving public key from keystore");
+        }
     }
 
     public String getUsernameFromJWT(String token) {
         Claims claims = Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(getPublicKey())
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
